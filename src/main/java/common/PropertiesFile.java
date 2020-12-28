8307dd930f140c478150d4231dd4fea27be39c4a
Sritaj <sritajp@gmail.com>
2020-12-28 20:03:46 +0530

    Added PropertiesFile class to create static methods for Properties file operation

---
+++ src/main/java/common/PropertiesFile.java
@@ -0,0 +1,27 @@
+package common;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertiesFile {
+
+    public static String readProperties(String key) throws IOException {
+
+        FileReader reader= null;
+        try {
+            reader = new FileReader(Constant.propertiesFilePath);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        Properties p=new Properties();
+        p.load(reader);
+
+        String value = p.getProperty(key);
+
+        return value;
+
+    }
+}
