da49f44adbe286dc744abafbd699e9b5f74ac25b
Sritaj <sritajp@gmail.com>
2020-12-28 20:08:12 +0530

    Sample test file

---
+++ src/main/java/scripts/testClass.java
@@ -0,0 +1,23 @@
+package scripts;
+
+import java.io.IOException;
+
+public class testClass {
+
+    public static void main(String[] args) throws IOException {
+
+        /*
+        String path = "/Users/sritaj/Downloads/LearnAtUdemy/SeleniumPOC/src/main/browsers-dependency/chromedriver";
+        System.setProperty("webdriver.chrome.driver", path);
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://accqa.aurigoessentials.com/");
+         */
+
+       // System.out.println(PropertiesFile.readProperties("fundAgency"));
+
+
+
+        //driver.get("www.google.com");
+
+    }
+}
