22d1f1e7168190a26c3dfca04b8d02f50484adfe
Sritaj <sritajp@gmail.com>
2020-12-29 20:35:03 +0530

    Added script to load data into User Accounts and added relevant files

--- src/main/java/scripts/LibraryDataLoadForUsers.java
+++ src/main/java/scripts/LibraryDataLoadForUsers.java
@@ -7,9 +7,7 @@ import org.junit.jupiter.api.*;
 
 import java.io.IOException;
 
-
-public class LibraryDataLoadForBudget {
-
+public class LibraryDataLoadForUsers {
 
     @BeforeAll
     static void setup(){
@@ -26,19 +24,19 @@ public class LibraryDataLoadForBudget {
 
     @Test
     @Order(1)
-    void loadDataInBudgetEstimateType() throws InterruptedException, IOException {
+    void loadDataInUserAccounts() throws InterruptedException, IOException {
 
         //Reading the key from config.properties file to get the form specific url
-        String budgetEstimateTypeURL = PropertiesFile.readProperties("budgetEstimateTypeURL");
+        String userAccountsURL = PropertiesFile.readProperties("userAccountsURL");
 
         //appending the build/env specific URL with the form URL
-        String completeFormURL = Constant.buildURL+budgetEstimateTypeURL;
+        String completeFormURL = Constant.buildURL+userAccountsURL;
 
         //Reading the key from config.properties file to get the form specific Excel File to upload
-        String budgetEstimateTypeExcelFile = PropertiesFile.readProperties("budgetEstimateTypeExcelFile");
+        String userAccountsExcelFile = PropertiesFile.readProperties("userAccountsExcelFile");
 
         //appending the excel file name at the end of the excel file absolute paths
-        String completeExcelFileURL =  Constant.excelSheetsFilePath +budgetEstimateTypeExcelFile;
+        String completeExcelFileURL =  Constant.excelSheetsFilePath +userAccountsExcelFile;
 
         //common method defined in the MWCommon class to redirect to particular form Upload page and perform operations
         MWCommon.excelSheetUpload(completeFormURL, completeExcelFileURL);
@@ -54,8 +52,10 @@ public class LibraryDataLoadForBudget {
 
     }
 
+
     @AfterAll
     static void terminate(){
         MWCommon.quit();
     }
+
 }
